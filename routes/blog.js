07fe494b706aec6d9abb5b2c73ac246fc4b71a5c
Michael Len <michael.len@ooyala.com>
2015-07-19 16:45:08 -0700

    add db table to blog

--- routes/blog.js
+++ routes/blog.js
@@ -3,13 +3,14 @@ var blogApi = require('node-mysql-blog-api');
 var blog = blogApi.api({
 host    : 'localhost',
 user    : 'root',
-database: 'lensite' });
+database: 'lensite',
+table   : 'blog_posts_test'});
 
 
 exports.index = function(req, res){
   res.set('Cache-Control', 'max-age=4320000');
     console.log("starting");
-  blog.posts({count:1}, function(err, posts) {
+  blog.posts({count:3}, function(err, posts) {
     console.log(JSON.stringify(posts));
     res.render('blog', { 'posts': posts });
   });
